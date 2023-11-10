03faf0dbd949321c3f17fedcc7840df0e8affe91
AlexandraMoldovan11 <149817008+AlexandraMoldovan11@users.noreply.github.com>
2023-11-11 01:03:28 +0200

    Add files via upload

---
+++ demo/Test.java
@@ -0,0 +1,10 @@
+
+
+@Data
+@NoArgsConstructor
+public class Test {
+
+    private String test1;
+    private int count;
+    private boolean isAdmin;
+}
---
+++ demo/src/main/java/dtos/Test.java
@@ -0,0 +1,15 @@
+//package dtos;
+package com.example.demo.dtos;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Test {
+
+    private String test1;
+
+    private int count;
+
+    private boolean isAdmin;
+}
